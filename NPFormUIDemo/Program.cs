23c1957f73cf2ee448f620248e0d1c51d2fa8a62
dxwang <4638912@qq.com>
2025-11-02 00:07:32 +0800

    a

--- NPFormUIDemo/Program.cs
+++ NPFormUIDemo/Program.cs
@@ -14,7 +14,7 @@ namespace ReaLTaiizor_CR
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form4());
         }
     }
 }
